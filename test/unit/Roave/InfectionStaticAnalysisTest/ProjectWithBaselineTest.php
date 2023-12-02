b303463a9a0d76c5bba5b7021612e27891c06fbd
Barney Laurance <barney@thebiggive.org.uk>
2023-12-02 11:03:46 +0000

    484: Add functional test to show how a high MSI is reported when Psalm issues exist in baseline

---
+++ test/unit/Roave/InfectionStaticAnalysisTest/ProjectWithBaselineTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\InfectionStaticAnalysisTest;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
+
+class ProjectWithBaselineTest extends TestCase
+{
+    public function setUp(): void
+    {
+        $process = new Process(['composer', 'update']);
+        $process->setWorkingDirectory(__DIR__ . '/../../../asset/ProjectWithPsalmBaseline');
+
+        $process->run();
+    }
+
+    public function testItSpuriouslyReportsMutantAsKilledWithPsalmBaseline(): void
+    {
+        $process = new Process([__DIR__ . '/../../../asset/ProjectWithPsalmBaseline/vendor/bin/roave-infection-static-analysis-plugin']);
+        $process->setWorkingDirectory(__DIR__ . '/../../../asset/ProjectWithPsalmBaseline');
+
+        $process->run();
+
+        $output = $process->getOutput();
+
+        $this->assertSame(0, $process->getExitCode());
+        $this->assertStringContainsString('4 mutants were killed', $output);
+        $this->assertStringContainsString('Mutation Score Indicator (MSI): 100%', $output);
+    }
+}
